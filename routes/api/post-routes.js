49a4b2374008f6144f735a059c8d6069da057aa0
Mitchell George <mitchgeorge8@gmail.com>
2022-05-08 12:05:41 -0400

    13.4.6 Refactor messy code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -87,29 +87,7 @@ router.post("/", (req, res) => {
 
 // Upvote a post
 router.put("/upvote", (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then(() => {
-      return Post.findOne({
-        where: {
-          id: req.body.post_id,
-        },
-        attributes: [
-          "id",
-          "post_url",
-          "title",
-          "created_at",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count",
-          ],
-        ],
-      });
-    })
+  Post.upvote(req.body, { Vote })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
