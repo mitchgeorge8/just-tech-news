970df434e8f86c4de99a5f3eb02f2d729dd7beac
Mitchell George <mitchgeorge8@gmail.com>
2022-05-14 21:30:21 -0400

    14.3.4 Add upvote functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -103,12 +103,17 @@ router.post("/", (req, res) => {
 
 // Upvote a post
 router.put("/upvote", (req, res) => {
-  Post.upvote(req.body, { Vote })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  if (req.session) {
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Update a post's title
