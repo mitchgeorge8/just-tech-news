42812bb30d465d472a1bccc7b6436946eceffa36
Mitchell George <mitchgeorge8@gmail.com>
2022-05-15 10:51:13 -0400

    14.4.7 Write and fulfill test for url formats

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const { format_date, format_plural } = require("../utils/helpers");
+const { format_date, format_plural, format_url } = require("../utils/helpers");
 
 test("fomat_date() returns a date string", () => {
   const date = new Date("2020-03-20 16:12:03");
@@ -6,10 +6,20 @@ test("fomat_date() returns a date string", () => {
   expect(format_date(date)).toBe("3/20/2020");
 });
 
-test("format_plural() correctly pluralizes words", () => {
+test("format_plural() returns a pluralized word", () => {
   const pluralWord = "Tiger";
   const singleWord = "Lion";
 
   expect(format_plural(pluralWord, 2)).toBe("Tigers");
   expect(format_plural(singleWord, 1)).toBe("Lion");
+});
+
+test("format_url() returns a simplified url string", () => {
+  const url1 = format_url("http://test.com/page/1");
+  const url2 = format_url("https://www.coolstuff.com/abcdefg/");
+  const url3 = format_url("https://www.google.com?q=hello");
+
+  expect(url1).toBe("test.com");
+  expect(url2).toBe("coolstuff.com");
+  expect(url3).toBe("google.com");
 });
