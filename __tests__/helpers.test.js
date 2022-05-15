8089a2f19053f20019b4902a6707d9a7336e0cf1
Mitchell George <mitchgeorge8@gmail.com>
2022-05-15 10:44:52 -0400

    14.4.6 Write and fulfill test for plural words

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,15 @@
-const { format_date } = require("../utils/helpers");
+const { format_date, format_plural } = require("../utils/helpers");
 
 test("fomat_date() returns a date string", () => {
   const date = new Date("2020-03-20 16:12:03");
 
   expect(format_date(date)).toBe("3/20/2020");
+});
+
+test("format_plural() correctly pluralizes words", () => {
+  const pluralWord = "Tiger";
+  const singleWord = "Lion";
+
+  expect(format_plural(pluralWord, 2)).toBe("Tigers");
+  expect(format_plural(singleWord, 1)).toBe("Lion");
 });
